2d92448fd83866e647c141130d54a5d6ab32efbe
bartoszOsn <osinskibartosz24@gmail.com>
2022-03-05 16:26:52 +0100

    TEST-APP add react

--- test-application/src/index.js
+++ test-application/src/index.js
@@ -1,1 +1,8 @@
+import * as React from 'react'
+import * as ReactDom from 'react-dom'
 
+function App() {
+	return React.createElement('h1', null, 'Hello!');
+}
+
+ReactDom.render(React.createElement(App, null, null), document.querySelector('#app'));
